563709c9349480084cc5d1c690968dee96a5035d
phob0z <leonel.alfonso@gmail.com>
2022-08-08 18:16:43 -0500

    Problemas con el css

--- resources/views/portafolio/edit.blade.php
+++ resources/views/portafolio/edit.blade.php
@@ -1,21 +1,3 @@
-<!DOCTYPE html>
-<html lang="es">
-
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Portafolio Grupo 1 - Twitter</title>
-    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-    <script src="js/app.js" defer></script>
-    <script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" crossorigin="anonymous"></script>
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
-    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet"
-        type="text/css" />
-    <link href="css/styles.css" rel="stylesheet" />
-</head>
 @extends('layouts.app')
 
 @section('content')
