56fa6a4b678af608de35c5b59ee3f33784a4d56c
phob0z <leonel.alfonso@gmail.com>
2022-08-09 16:09:28 -0500

    Login with Twitter

--- app/Http/Controllers/SugerenciasController.php
+++ app/Http/Controllers/SugerenciasController.php
@@ -26,19 +26,10 @@ class SugerenciasController extends Controller
             'required'=>'El campo :attribute es obligatorio',
             'mensaje.min' =>'El :attribute debe tener mas de 5 caracteres'
         ]);
-
-        //return request()->all();
-        //notify()->success('Su sugerencia sera tomada en cuenta');
-        Mail::to('leonel_alfonso@hotmail.com')->queue(new Sugerencias($mensaje));
-        Mail::to('leonel.alfonso@gmail.com')->queue(new Sugerencias($mensaje));
         Mail::to('leonel.molina@epn.edu.ec')->queue(new Sugerencias($mensaje));
-        //Mail::to('tenorio0967@hotmail.com')->queue(new Sugerencias($mensaje));
-        //Mail::to('jean.fuentes@epn.edu.ec')->queue(new Sugerencias($mensaje));
-        //Mail::to('jean.pierre1504@hotmail.com')->queue(new Sugerencias($mensaje));
-        //Mail::to('jeanpyfuentes23@gmail.com')->queue(new Sugerencias($mensaje));
-        //Mail::to('byron.loarteb@epn.edu.ec')->queue(new Sugerencias($mensaje));
-        //Mail::to('by_tosh20@hotmail.com')->queue(new Sugerencias($mensaje));
-        //Mail::to('bloarte@entodositio.com')->queue(new Sugerencias($mensaje));
+        Mail::to('byron.loarteb@epn.edu.ec')->queue(new Sugerencias($mensaje));
+        Mail::to('by_tosh20@hotmail.com')->queue(new Sugerencias($mensaje));
+        Mail::to('bloarte@entodositio.com')->queue(new Sugerencias($mensaje));
         return view('sugerencias.mensajeEnviado');
     }
 }
